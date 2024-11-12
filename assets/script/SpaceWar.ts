0a422dd5813e62168d8f56f3d8c1e48548870c62
Baocheng Wang <baocheng0121@gmail.com>
2024-11-12 22:10:53 +1100

    - added tank to main script

--- assets/script/SpaceWar.ts
+++ assets/script/SpaceWar.ts
@@ -1,3 +1,5 @@
+import Tank from "./Tank";
+
 const {ccclass, property} = cc._decorator;
 
 @ccclass
@@ -5,5 +7,8 @@ export default class SpaceWar extends cc.Component {
     
     @property(cc.Node)
     private background: cc.Node = undefined as never;
+
+    @property(Tank)
+    private tank: Tank = undefined as never;
 
 }
